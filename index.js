41f5d9b6a667df52ac2a8ea7c5d82c1625949363
jotadev <mejiajhoan2021@gmail.com>
2024-08-25 13:50:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const form = document.querySelector("form")
+let script = ""
+
+new Bell({
+    description: "Llama la atencion completa del usuario",
+},"error",{
+    screenTime:1000000,distance:{y:100},
+    theme: "colors",
+    distance:{
+        x:0
+    }
+}).launch()
+new Bell({
+    title: "Nuevo tema Colors",
+    description: "Llama la atencion completa del usuario",
+},"warning",{
+    screenTime:1000000,distance:{y:100},
+    theme: "colors",
+    distance:{
+        x:0
+    }
+}).launch()
+new Bell({
+    title: "Nuevo tema Colors",
+    description: "Llama la atencion completa del usuario",
+},"success",{
+    screenTime:1000000,distance:{y:100},
+    theme: "colors",
+    distance:{
+        x:0
+    }
+}).launch()
+new Bell({
+    title: "Nuevo tema Colors",
+},"info",{
+    screenTime:1000000,distance:{y:100},
+    theme: "colors",
+    distance:{
+        x:0
+    }
+}).launch()
+
+form.addEventListener("submit",e=>{
+    e.preventDefault()
+    const $firstParams = [...document.querySelectorAll("form div:nth-child(1) input")]
+    const $secondParam = [...document.querySelectorAll("form > div:nth-child(2) input")].find(i => i.checked)
+    const $threeParam = [...document.querySelectorAll("form > div:nth-child(3) input, textarea")].filter(i => i.checked || (i.value && (i.type !== 'radio')))
+    const first = "{\n" + $firstParams.filter(i => i.value).map($param => `${$param.name}: '${$param.value}'`).join(",\n") + "\n},"
+    const second = "'"+$secondParam.value+"',"
+    const three = "{\n" + $threeParam.filter(i => i.value).map($param => `${$param.name}: ${($param.checked && $param.value == "on" ? true : false) || ($param.value !== "on" ? "'"+$param.value+"'": false)}`).join(",\n") + "\n}"
+    const body = "const params = [" + first + "\n" + second + "\n" + three +"]"
+    const head = "const bell = new Bell(...params)"
+    const launch = "bell.launch()"
+
+
+    script = body + "\n" + head +"\n" + launch
+    eval(script)
+    document.querySelector("#placeholder").innerHTML = `<code class="languaje-javascript atom-one-dark">${script}</code>`
+    hljs.highlightAll()
+})
+
+document.querySelector("#copy").addEventListener("click",()=>{
+    navigator.clipboard.writeText(script)
+})
